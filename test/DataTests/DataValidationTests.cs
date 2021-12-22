5940d5a321bfbb458a4673e89bb3e6cef5f74f67
NicoJuicy <NicoJuicy@DESKTOP-AN8UQHQ>
2021-12-22 04:22:03 +0100

    Add data integration tests

--- test/DataTests/DataValidationTests.cs
+++ test/DataTests/DataValidationTests.cs
@@ -11,36 +11,53 @@ namespace DataTests
     [TestClass]
     public class DataValidationTests
     {
-        private static string TRAIN_DATA_FILEPATH = "";
+        private static string TRAIN_DATA_FILEPATH = @"C:\Users\NicoJuicy\AppData\Local\Temp\mlnet-workshop\data\true_car_listings.csv";
+
+        private static IEnumerable<global::Shared.modelInput> Rows;
 
         [ClassInitialize]
         public static void Initialize(TestContext testContext)
         {
+            var mlContext = new MLContext();
+            var data = mlContext.Data.LoadFromTextFile<global::Shared.modelInput>(TRAIN_DATA_FILEPATH, hasHeader: true, separatorChar: ',');
 
+            Rows = mlContext.Data.CreateEnumerable<global::Shared.modelInput>(data, false);
         }
 
-        [TestMethod]
-        public void VerifyValidMilage()
-        {
-
-        }
 
         [TestMethod]
         public void VerifyValidPrice()
         {
+            var hasNegativePrice = Rows.Any(x => x.Price < 0);
 
+            hasNegativePrice.Should().BeFalse();
         }
 
         [TestMethod]
         public void VerifyValidYear()
         {
+            var hasValidYears = Rows.All(x => x.Year > 1950 && x.Year < DateTime.Now.Year + 1);
 
+            hasValidYears.Should().BeTrue();
+        }
+        [TestMethod]
+        public void VerifyValidMilage()
+        {
+            var hasInvalidMilage = Rows.Any(x => x.Mileage < 0);
+
+            hasInvalidMilage.Should().BeFalse();
         }
 
         [TestMethod]
         public void VerifyMinimumNumberOfRows()
         {
+            var rowCount = Rows.Count();
 
+            rowCount.Should().BeGreaterThan(10000);
         }
+
+
+
+
     }
 }
